13d757fa0c5dc8af1a2ff0e0230a6ecc963b87ed
Null Builds <12244473+nullbuilds@users.noreply.github.com>
2025-06-09 10:06:35 -0500

    feat: add dependency injector
    Adds a basic dependency injector implementation.

---
+++ scripts/inject/injection_context.gd
@@ -0,0 +1,7 @@
+## Helper for setting the bindings on an injector.
+class_name InjectionContext
+extends Object
+
+## Configures the provided binder.
+func configure_bindings(_binder: InjectionBinder) -> void:
+	assert(false, "Not implemented")
