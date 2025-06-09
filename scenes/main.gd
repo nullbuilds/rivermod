91b7e05af433f7a944c694165e66c1fd2afe9ffd
Null Builds <12244473+nullbuilds@users.noreply.github.com>
2025-06-09 11:42:08 -0500

    feat: add editor configuration service
    Adds an editor configuration service.

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,11 +6,18 @@ var _injector: Injector = null
 
 ## Construct the main scene.
 func _ready() -> void:
+	get_tree().set_auto_accept_quit(false)
 	_injector = Injector.create(ApplicationInjectionContext.new())
-	
-	_load_editor_configuration()
 
 
-## Preloads the editor configuration
-func _load_editor_configuration() -> void:
-	_injector.provide(EditorConfigurationSource).reload()
+## Handles incoming notifications.
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		_close_application(0)
+
+
+## Close the application and return the given exit code.
+func _close_application(exit_code: int) -> void:
+	var config_service: EditorConfigurationService = _injector.provide(EditorConfigurationService)
+	config_service.save()
+	get_tree().quit(exit_code)
