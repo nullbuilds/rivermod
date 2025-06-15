96d58b8b04111ab8fea39e2a2213227c60c2605a
Charles <CharlesETD@gmail.com>
2025-06-14 23:43:53 -0500

    chore: update abstract class parameters
    Updates the abstract class parameters to not use unused variable names
    to avoid warnings and instead just suppress the warnings. This allows
    auto-complete to not look werird.

--- scripts/editor_config/editor_configuration_source.gd
+++ scripts/editor_config/editor_configuration_source.gd
@@ -6,13 +6,15 @@ extends Object
 ## 
 ## When an existing value is not present, the provided default will be set as
 ## the value going forward unless it too is null.
-func get_configuration(_context: String, _key: String, _default: Variant = null) -> Variant:
+@warning_ignore("unused_parameter")
+func get_configuration(context: String, key: String, default: Variant = null) -> Variant:
 	assert(false, "Not implemented")
 	return null
 
 
 ## Sets the specificied configuration key to the given value.
-func set_configuration(_context: String, _key: String, _value: Variant) -> void:
+@warning_ignore("unused_parameter")
+func set_configuration(context: String, key: String, value: Variant) -> void:
 	assert(false, "Not implemented")
 
 
