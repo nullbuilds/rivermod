6c8066ddc392a3137561222d5bf5a9dcd6cd90e5
Charles <CharlesETD@gmail.com>
2025-06-15 22:08:43 -0500

    feat: add editor file source
    Adds an abstraction layer for accessing editor files.

--- scripts/inject/application_injection_context.gd
+++ scripts/inject/application_injection_context.gd
@@ -10,6 +10,19 @@ func configure_bindings(binder: InjectionBinder) -> void:
 	binder.bind(EditorConfigurationService, _provide_editor_configuration_service)
 	binder.bind(GameFileSource, _provide_game_file_source)
 	binder.bind(GameSaveDataRepository, _provide_game_save_data_repository)
+	binder.bind(EditorFileSource, _provide_editor_file_source)
+
+
+## Provides an EditorFileSource
+func _provide_editor_file_source(injector: Injector) -> EditorFileSource:
+	var protected_file_paths: PackedStringArray = PackedStringArray()
+	protected_file_paths.append(_EDITOR_CONFIG_FILE_PATH)
+	protected_file_paths.append("logs")
+	protected_file_paths.append("shader_cache")
+	protected_file_paths.append("vulkan")
+	
+	return FileSystemEditorFileSource.new(injector.provide(EditorConfigurationService),
+			protected_file_paths)
 
 
 ## Provides a GameSaveDataRepository
