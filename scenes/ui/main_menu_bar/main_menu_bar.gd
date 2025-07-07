ad1bca4496a0a036fdef9ee0400cf9c218ff68a3
Null Builds <12244473+nullbuilds@users.noreply.github.com>
2025-07-07 00:10:46 -0500

    feat: add main menu bar
    Adds an initial widget for the main menu bar.

---
+++ scenes/ui/main_menu_bar/main_menu_bar.gd
@@ -0,0 +1,35 @@
+class_name MainMenuBar
+extends MarginContainer
+## The editor's main menu bar.
+
+## Emitted when the user selects the help documentation option.
+signal editor_help_documentation_pressed()
+
+## Emitted when the user chooses the option to change the game directory.
+signal editor_game_directory_pressed()
+
+const _EDITOR_GAME_DIRECTORY_ID: int = 0
+const _HELP_DOCUMENTATION_ID: int = 0
+
+@onready var _editor_popup_menu: PopupMenu = %EditorPopupMenu
+@onready var _help_popup_menu: PopupMenu = %HelpPopupMenu
+
+## Readies the component.
+func _ready() -> void:
+	_editor_popup_menu.id_pressed.connect(_on_editor_id_pressed)
+	_help_popup_menu.id_pressed.connect(_on_help_id_pressed)
+
+
+## Called when an editor menu item is pressed.
+func _on_editor_id_pressed(id: int) -> void:
+	match(id):
+		_EDITOR_GAME_DIRECTORY_ID:
+			editor_game_directory_pressed.emit()
+			
+
+
+## Called when a help menu item is pressed.
+func _on_help_id_pressed(id: int) -> void:
+	match(id):
+		_HELP_DOCUMENTATION_ID:
+			editor_help_documentation_pressed.emit()
